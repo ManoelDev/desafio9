6690e5ac471bb8d3410bb9479ca379e9a8f4de1b
Manoe Neto <maguserv@gmail.com>
2020-09-24 23:33:32 -0300

    Final challenge 9

---
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+
+import CreateCustomerService from '@modules/customers/services/CreateCustomerService';
+
+import { container } from 'tsyringe';
+
+export default class CustomersController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    // TODO
+    const { name, email } = request.body;
+    const createCustomer = container.resolve(CreateCustomerService);
+    const constumer = await createCustomer.execute({ name, email });
+    return response.json(constumer);
+  }
+}
